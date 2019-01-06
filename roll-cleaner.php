88ebf929ecd62e7453ab09db069ecefbd1a0fd51
S R Prescott <sararprescott@yahoo.com>
2019-01-06 01:10:36 -0500

    SEO

--- roll-cleaner.php
+++ roll-cleaner.php
@@ -11,7 +11,12 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Yak Store</title>
+        
+        <title>Yak Store | Construction Grade Tools For Any Project</title>
+        <meta name="description" content="Construction grade tools for any job or project. Our products will make your job easier in so many ways.">
+        <meta name="keywords" content="Drill Dress, Dust Wall, Clamp Extension Tool, Paint Roll Cleaner, Home Improvement, Remodel, Remodeling, Construction, Project, Job, Clean Up, Job Site, Tools, Construction, Contractors">
+        
+        
         <link rel="stylesheet" href="main.css">
         <link rel="stylesheet" href="roll-cleaner.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,7 +40,7 @@
         <div class="container">
             <div class="row">
                 <section class="header" id="about">
-                    <div class="ban"> <img class="banner" src="images/banner-4.jpg"> </div>
+                    <div class="ban"> <img class="banner" src="images/yak-construction-products.jpg" alt="YAK Construction Products"> </div>
                     <div class="topnav" id="myTopnav"> <a href="index.html">Yak Home</a> <a href="dust-wall.html">Dust Wall</a> <a href="drill-press.html">Drill Press</a> <a href="ex-tool.html">Extention Tool</a> <a href="roll-cleaner.php">Paint Roll Cleaner</a> <a href="contact-us.html">Contact Us</a>
                         <p class="this-page">Paint Roll Cleaner</p> <a href="javascript:void(0);" style="font-size:20px;" class="icon" onclick="myFunction()">&#9776;</a> </div>
                 </section>
@@ -43,7 +48,11 @@
                     <div class="area-one">
                         <div class="intro">
                             <p class="p-heading">Meet The YAK Paint Roll Cleaner!</p>
-                            <p class="p-body">The YAK Paint Roll Cleaner is a versatile, easy-to-use cleaner for any size paint roll. It will cut your clean up time to a fraction of what you normally spend cleaning up after a painting project. It confines the mess to a small area, and contains any potential splatter mess!</p> <img class="image" src="images/roll-cleaner/YAK-still.jpg">
+                            <p class="p-body">The YAK Paint Roll Cleaner is a versatile, easy-to-use cleaner for any size paint roll. It will cut your clean up time to a fraction of what you normally spend cleaning up after a painting project. It confines the mess to a small area, and contains any potential splatter mess!</p> 
+                            
+                            <img class="image" src="images/roll-cleaner/paint-roll-cleaner-3.jpg" alt="YAK Paint Roll Cleaner">
+                            <img class="image" src="images/roll-cleaner/paint-roll-cleaner-2.jpg" alt="YAK Paint Roll Cleaner">
+                            
                             <p class="p-body">After a long day of painting, the last thing you want to do is spend hours cleaning your paint rolls for storage. Some people even throw them away to avoid the hassle. Instead, use the YAK Paint Roll Cleaner to clean each paint roll in under two minutes!</p>
                         </div>
                     </div>
@@ -56,13 +65,15 @@
                                
                                 <p class="caption">Watch the YAK Paint Roll Cleaner fully clean a large paint roll in less that 30 seconds!</p>
                             </div>
-                            <!-- <div class="video">
-                                <iframe src="https://www.youtube.com/embed/IO1RSLy2Vog" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            <div class="video">
+                                <iframe src="https://www.youtube.com/embed/nONbZX35PYo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 <p class="caption">See how the YAK Paint Roll Cleaner works it's magic!</p>
-                            </div> --></div>
+                            </div>
+                            
+                        </div>
                     </div>
                     <div class="buy-now">
-                        <p class="p-heading-two white-text">Find Out When YOU Can Buy the YAK Paint Roll Cleaner!</p> <img class="yak" src="images/roll-cleaner/product-1.jpg">
+                        <p class="p-heading-two white-text">Find Out When YOU Can Buy the YAK Paint Roll Cleaner!</p> <img class="yak" src="images/roll-cleaner/paint-roll-cleaner-product.jpg" alt="YAK Paint Roll Cleaner">
                         <h4 class="white-text">The YAK Paint Roll Cleaner is not currently being sold. Please enter your name and email address below to receive an email when this product is ready for purchase!</h4>
                         <div class="button">
                             <form method="post">
